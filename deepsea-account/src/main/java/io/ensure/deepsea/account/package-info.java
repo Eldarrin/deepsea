c56d7e7e015280bd529bea158d8ec31084832f66
andy <andy@fluffy>
2018-10-28 13:50:03 +0000

    removed account

--- deepsea-account/src/main/java/io/ensure/deepsea/account/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Indicates that this module contains classes that need to be generated / processed.
- */
-@ModuleGen(name = "deepsea-user-account", groupPackage = "io.ensure.deepsea.account")
-package io.ensure.deepsea.account;
-
-import io.vertx.codegen.annotations.ModuleGen;
