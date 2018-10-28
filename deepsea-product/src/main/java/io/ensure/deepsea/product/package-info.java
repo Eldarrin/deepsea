666430ecb54effff08f685b637fb094924a02aff
andy <andy@fluffy>
2018-10-28 13:47:14 +0000

    removed deepsea-product

--- deepsea-product/src/main/java/io/ensure/deepsea/product/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Indicates that this module contains classes that need to be generated / processed.
- */
-@ModuleGen(name = "deepsea-product", groupPackage = "io.ensure.deepsea.product")
-package io.ensure.deepsea.product;
-
-import io.vertx.codegen.annotations.ModuleGen;
